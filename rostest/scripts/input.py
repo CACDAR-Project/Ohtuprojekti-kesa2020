d16e84bcc9f11a39adc5533bf866555d148278a2
MelDv <maaret.dufva@helsinki.fi>
2020-06-03 15:51:14 +0300

    Generate automatic documentation

--- rostest/scripts/input.py
+++ rostest/scripts/input.py
@@ -1,71 +1,28 @@
 #!/usr/bin/env python3.7
-from rostest.srv import text_message, text_messageResponse, new_frequency, new_frequencyResponse
+from rostest.srv import text_message, text_messageResponse
 import rospy
 
 # https://github.com/ros/ros_tutorials/blob/noetic-devel/rospy_tutorials/005_add_two_ints/add_two_ints_client
 
-message_receiver = None
-frequency_changer = None
-qr_frequency_changer = None
-
-
-def send_message():
-    print("Give message!")
-    inp = input()
-    response = message_receiver(inp)
-    print("Received response: " + response.response)
-
-
-def send_frequency():
-    print("Give frequency!")
-    inp = int(input())
-    response = frequency_changer(inp)
-    print("Received response: " + response.response)
-
-
-def send_qr_frequency():
-    print("Give frequency!")
-    inp = int(input())
-    response = qr_frequency_changer(inp)
-    print("Received response: " + response.response)
-
+# @TODO: ### **Fix input: not working on docker.**
+# - [ ] testing
 
 def run():
+
+    print("Waiting for service")
+    rospy.wait_for_service('inputs')
+    print("Service found")
+
     while not rospy.is_shutdown():
-        print(
-            "Give command.\n1 for sending message, 2 for changing object detection frequency, "
-            + "3 for changing QR code detection frequency:")
+        message_reciever = rospy.ServiceProxy('inputs', text_message)
+
+        print("Type your message!")
         inp = input()
+        print("You typed: " + inp)
 
-        # Catch errors if a node is not running
-        try:
-            if inp == "1":
-                send_message()
-            elif inp == "2":
-                send_frequency()
-            elif inp == "3":
-                send_qr_frequency()
-            else:
-                print("Command not recognized!")
-        except Exception as ex:
-            print("Got exception:", ex)
-
-
-def init():
-    print("Waiting for services")
-    # rospy.wait_for_service('inputs')
-    # rospy.wait_for_service('frequency')
-    print("Service founds")
-
-    global message_receiver
-    global frequency_changer
-    global qr_frequency_changer
-
-    message_receiver = rospy.ServiceProxy('inputs', text_message)
-    frequency_changer = rospy.ServiceProxy('frequency', new_frequency)
-    qr_frequency_changer = rospy.ServiceProxy('qr_frequency', new_frequency)
+        response = message_reciever(inp)
+        print("Recieved response: " + response.response)
 
 
 if __name__ == "__main__":
-    init()
     run()
