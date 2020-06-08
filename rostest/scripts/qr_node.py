7e9a3d2867f4e17a87e2a65121e1e38d3f7bbd2f
Teo44 <43036149+Teo44@users.noreply.github.com>
2020-06-08 15:10:38 +0300

    changing QR detection frequency from input node

--- rostest/scripts/qr_node.py
+++ rostest/scripts/qr_node.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3.7
 import rospy
+import time
 from image_converter import ImageConverter
 from qr_detector import QrDetector
 from std_msgs.msg import String
 from rostest.msg import image, qr_observation, polygon, boundingbox
+from rostest.srv import new_frequency, new_frequencyResponse
 
+# Frequency in hertz
+qr_run_frequency = 10
+qr_period = 1.0 / qr_run_frequency
 
 class QRReader:
+    def change_frequency(self, new_frequency):
+        global qr_run_frequency
+        global qr_period
+        qr_run_frequency = new_frequency.data
+        qr_period = 1.0 / qr_run_frequency
+        return new_frequencyResponse("QR detector received you frequency!")
+
     def __init__(self, on: bool = False):
         self.detect_on = on
         self.converter = ImageConverter()
@@ -18,7 +30,15 @@ class QRReader:
         # Camera feed is read from ros messages
         self.input_sub = rospy.Subscriber("camera_feed", image, self.detect)
 
+        # ROS service for changing detection frequency.
+        frequency_service = rospy.Service('qr_frequency', new_frequency,
+                                      self.change_frequency)
+
+
+
     def detect(self, msg: image):
+        # For tracking the frequency
+        start_time = time.time()
         # Only run the detection if it is turned on
         if not self.detect_on:
             return
@@ -38,6 +58,11 @@ class QRReader:
                         o["polygon"][2]["x"], o["polygon"][2]["y"],
                         o["polygon"][3]["x"], o["polygon"][3]["y"]))
             self.pub.publish(obs)
+        
+        time_to_sleep = qr_period - (time.time() - start_time)
+        if time_to_sleep > 0:
+            time.sleep(time_to_sleep)
+        return
 
 
 if __name__ == "__main__":
