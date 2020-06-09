d6168889644bd7433fc4952f3d2c5bb7ce35b6bd
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2020-06-09 19:10:43 +0300

    Make qr node thread-safe

--- rostest/scripts/qr_node.py
+++ rostest/scripts/qr_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.7
 import rospy
+import threading
 import time
 import image_converter
 import qr_detector
@@ -7,18 +8,32 @@ from std_msgs.msg import String
 from rostest.msg import image, qr_observation, polygon, boundingbox, point64
 from rostest.srv import new_frequency, new_frequencyResponse
 
-# Frequency in hertz
-qr_run_frequency = 10
-qr_period = 1.0 / qr_run_frequency
-
 
 class QRReader:
+    # Frequency in hertz
+    qr_run_frequency = 10
+    qr_period = 1.0 / qr_run_frequency
+
+    last_detect = 0  # Last time detect() was called
+    detect_lock = threading.Lock(
+    )  # Locked, while detect() is being run, we detect() only 1 image at time
+
+    frequency_change_lock = threading.Lock()
+
     def change_frequency(self, new_frequency):
-        global qr_run_frequency
-        global qr_period
-        qr_run_frequency = new_frequency.data
-        qr_period = 1.0 / qr_run_frequency
-        return new_frequencyResponse("QR detector received you frequency!")
+        with self.frequency_change_lock:
+            self.qr_run_frequency = new_frequency.data
+            self.qr_period = 1.0 / self.qr_run_frequency
+            return new_frequencyResponse(
+                "QR detector freq set to {}, period {}".format(
+                    self.qr_run_frequency, self.qr_period))
+
+    def receive_img(self, msg: image):
+        # Detect from this image, if not already detecting from another image and within period time constraints
+        if self.detect_on and (
+                time.time() - self.last_detect
+        ) > self.qr_period and self.detect_lock.acquire(False):
+            self.detect(msg)
 
     def __init__(self, on: bool = False):
         self.detect_on = on
@@ -27,7 +42,8 @@ class QRReader:
         self.pub = rospy.Publisher("qr_results", qr_observation, queue_size=50)
 
         # Camera feed is read from ros messages
-        self.input_sub = rospy.Subscriber("camera_feed", image, self.detect)
+        self.input_sub = rospy.Subscriber("camera_feed", image,
+                                          self.receive_img)
 
         # ROS service for changing detection frequency.
         frequency_service = rospy.Service('qr_frequency', new_frequency,
@@ -35,10 +51,8 @@ class QRReader:
 
     def detect(self, msg: image):
         # For tracking the frequency
-        start_time = time.time()
-        # Only run the detection if it is turned on
-        if not self.detect_on:
-            return
+        self.last_detect = time.time()
+        period = self.qr_period
 
         # Convert image from Image message to numpy ndarray
         img = image_converter.msg_to_cv2(msg)
@@ -58,10 +72,14 @@ class QRReader:
                 ]))
             self.pub.publish(obs)
 
-        time_to_sleep = qr_period - (time.time() - start_time)
-        if time_to_sleep > 0:
-            time.sleep(time_to_sleep)
-        return
+        processing_time = time.time() - self.last_detect
+        if processing_time > period:
+            print("Detecting QR-code took {}, while the period was set to {}!".
+                  format(processing_time,
+                         period))  # TODO: Announce "warnings" to topic
+
+        # Ready to detect the next image
+        self.detect_lock.release()
 
 
 if __name__ == "__main__":
