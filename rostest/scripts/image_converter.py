888a40d775ad47c4589a0592affae42584147075
Teo44 <43036149+Teo44@users.noreply.github.com>
2020-06-09 13:12:00 +0300

    merge fix

--- rostest/scripts/image_converter.py
+++ rostest/scripts/image_converter.py
@@ -3,11 +3,11 @@ from rostest.msg import image
 import cv2
 
 
-def cv2_to_msg(self, img: np.ndarray) -> image:
+def cv2_to_msg(img: np.ndarray) -> image:
     # ROS messages need one-dimensional lists as arguments
     return image(img.tobytes(), img.shape[0], img.shape[1])
 
-def msg_to_cv2(self, msg: image) -> np.ndarray:
+def msg_to_cv2(msg: image) -> np.ndarray:
     # Convert the channels from bytes to uint8 arrays and reconstruct the shape
     return np.frombuffer(msg.data,
                             "uint8").reshape(msg.height, msg.width, 3)
