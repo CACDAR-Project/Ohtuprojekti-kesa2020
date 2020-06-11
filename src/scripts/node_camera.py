c0045a0c99b341189d42a6f629bc1e1c0ba3a666
Joakim <joakim.joensuu@gmail.com>
2020-06-11 15:34:43 +0300

    Added doxygen documentation for image_converter and node_object_detector

--- src/scripts/node_camera.py
+++ src/scripts/node_camera.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.7
+## @package scripts
+
 import cv2 as cv
 import rospy
 from numpy import ndarray
@@ -6,10 +8,13 @@ from numpy import ndarray
 from konenako.msg import image
 from helpers.image_converter import cv2_to_msg
 
+##Global variable containing path to video, picture or camera device
 source = "/dev/video0"
 #source = "test.mp4"
 
-
+## Main function for camera node.
+#  Creates topic named "node_name/images" where image/video source 
+#  is being published as a image message.
 def run():
     rospy.init_node("camera")
     # We only want to process the latest frame, and Publisher's queue is
@@ -23,7 +28,6 @@ def run():
         # Converting the image from numpy ndarray to an image message
         msg = cv2_to_msg(img)
         pub.publish(msg)
-
 
 if __name__ == "__main__":
     run()
