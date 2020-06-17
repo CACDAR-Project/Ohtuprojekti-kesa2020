48fb66d4e7dcde272d077b5ce5fbc950525fcaab
Teo44 <43036149+Teo44@users.noreply.github.com>
2020-06-17 15:50:56 +0300

    input node fix and adding detectors

--- src/scripts/node_input.py
+++ src/scripts/node_input.py
@@ -3,7 +3,7 @@
 ## Provides functionality to send messages to different nodes using a terminal
 #  @package scripts
 
-from konenako.srv import new_frequency, toggle
+from konenako.srv import new_frequency, toggle, add_object_detector, remove_object_detector, add_object_detectorResponse, remove_object_detectorResponse
 import rospy
 
 # https://github.com/ros/ros_tutorials/blob/noetic-devel/rospy_tutorials/005_add_two_ints/add_two_ints_client
@@ -18,9 +18,11 @@ object_detection_toggler = None
 
 ## Sends a new frequency with frequency_changer and prints the received response.
 def send_frequency():
+    print("Give name of the detector:")
+    name = input()
     print("Give frequency!")
     inp = int(input())
-    response = frequency_changer(inp)
+    response = rospy.ServiceProxy(name + '/frequency', new_frequency)(inp)
     print("Received response: " + response.response)
 
 
@@ -34,10 +36,12 @@ def send_qr_frequency():
 
 ## Sends on or off command for object detector
 def send_OD_toggle():
+    print("Give name of the detector:")
+    name = input()
     print("Give something for on, empty for off!")
     inp = bool(input())
     print(inp)
-    response = object_detection_toggler(inp)
+    response = rospy.ServiceProxy(name + '/toggle', toggle)(inp)
     print("Received response: " + response.response)
 
 
@@ -49,6 +53,23 @@ def send_combine_toggle():
     response = combine_toggler(inp)
     print("Received response: " + response.response)
 
+## Sends parameters for adding a new detector
+def send_detector_add():
+    print("Give name for the new detector:")
+    name = input()
+    print("Give model path:")
+    model_path = input()
+    print("Give label file path:")
+    label_file = input()
+    detector_adder(name, model_path, label_file)
+
+
+## Sends name of a detector to be removed
+def send_detector_remove():
+    print("Give name of the detector to remove:")
+    name = input()
+    detector_remover(name)
+
 
 ## Function running in loop waiting for command to send message
 def run():
@@ -57,7 +78,9 @@ def run():
               "1 for changing object detection frequency,\n" +
               "2 for changing QR code detection frequency,\n" +
               "3 for toggling object detection on or off,\n" +
-              "4 for toggling result combining:")
+              "4 for toggling result combining,\n" +
+              "5 for adding detectors,\n" +
+              "6 for removing detectors:")
         inp = input()
 
         # Catch errors if a node is not running
@@ -70,6 +93,10 @@ def run():
                 send_OD_toggle()
             elif inp == "4":
                 send_combine_toggle()
+            elif inp == "5":
+                send_detector_add()
+            elif inp == "6":
+                send_detector_remove()
             else:
                 print("Command not recognized!")
         except Exception as ex:
@@ -80,45 +107,33 @@ def run():
 def init():
     rospy.init_node("input")
 
-    print(
-        "Are you running nodes from detector control node or separately? (d/s)"
-    )
-    while True:
-        inp = input()
-        if inp == "d":
-            oNode = "/object_detector"
-            qrNode = "/QR"
-            break
-        elif inp == "s":
-            oNode = "/object_detector"
-            qrNode = "/qr_detector"
-            break
-        else:
-            print("Command not recognized!")
+    oNode = "/object_detector"
+    qrNode = "/QR"
+    cNode = "/detector_control_node"
 
     print("Waiting for services")
 
-    rospy.wait_for_service(oNode + '/frequency')
-    print("Object detector frequency service found")
-    rospy.wait_for_service(qrNode + '/frequency')
-    print("QR detector frequency service found")
-    rospy.wait_for_service(oNode + '/toggle')
-    print("Object detection toggle service found")
-    if inp == "d":
-        rospy.wait_for_service('/detector_control_node/combine_toggle')
-        print("Combine toggle service found")
-        global combine_toggler
-        combine_toggler = rospy.ServiceProxy(
-            '/detector_control_node/combine_toggle', toggle)
+    # rospy.wait_for_service(oNode + '/frequency')
+    # print("Object detector frequency service found")
+    # rospy.wait_for_service(qrNode + '/frequency')
+    # print("QR detector frequency service found")
+    # rospy.wait_for_service(oNode + '/toggle')
+    # print("Object detection toggle service found")
 
-    global frequency_changer
+    # rospy.wait_for_service('/detector_control_node/combine_toggle')
+    # print("Combine toggle service found")
+    global combine_toggler
+    combine_toggler = rospy.ServiceProxy(
+        cNode + '/combine_toggle', toggle)
+
     global qr_frequency_changer
-    global object_detection_toggler
+    global detector_adder
+    global detector_remover
 
-    frequency_changer = rospy.ServiceProxy(oNode + '/frequency', new_frequency)
-    qr_frequency_changer = rospy.ServiceProxy(qrNode + '/qr_frequency',
+    qr_frequency_changer = rospy.ServiceProxy(qrNode + '/frequency',
                                               new_frequency)
-    object_detection_toggler = rospy.ServiceProxy(oNode + '/toggle', toggle)
+    detector_adder = rospy.ServiceProxy(cNode + "/add_object_detector", add_object_detector)
+    detector_remover = rospy.ServiceProxy(cNode + "/remove_object_detector", remove_object_detector)
 
 
 if __name__ == "__main__":
