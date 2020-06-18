1d30bd543151298cf127ac986efbfbfa7017416a
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2020-06-18 10:36:59 +0300

    Add more constants

--- src/scripts/config/constants.py
+++ src/scripts/config/constants.py
@@ -7,3 +7,27 @@ resources_path = 'resources'
 tflite_path = f'{resources_path}/tflite_models'
 videos_path = f'{resources_path}/videos'
 images_path = f'{resources_path}/images'
+
+
+# # Work in progress, these will change when the other PR is merged
+# # Nodes
+# camera = 'camera'
+# object_detector = 'object_detector'
+# qr_detector = 'qr_detector'
+
+# # Parameters
+# video_source = 'video_source'
+# video_feed_name = 'video_feed_name'
+# detection = 'detect_on'
+# model_file = 'model_file'
+# label_file = 'label_file'
+# frequency = 'frequency'
+
+# # Topics
+# images = 'images'
+# observations = 'observations'
+# warnings = 'warnings'
+
+# # Services
+# frequency = 'frequency'
+# toggle = 'toggle'
