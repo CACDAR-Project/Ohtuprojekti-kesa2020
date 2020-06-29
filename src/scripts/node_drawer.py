9d2bd8209cabe1a7034990d3e4289f8fe0fc825c
CircleCI <app@circleci.com>
2020-06-29 13:58:01 +0000

    "Merge pull request #204 from Konenako/drawer_node" formatted by CircleCI

--- src/scripts/node_drawer.py
+++ src/scripts/node_drawer.py
@@ -6,27 +6,27 @@ from konenako.msg import image, observations
 from helpers.image_converter import msg_to_cv2
 from config.constants import name_node_camera, topic_images, name_node_detector_control, topic_observations
 
-
 latest_observations = observations(0, 0, [])
 
 obs_lock = threading.Lock()
 draw_lock = threading.Lock()
 
+
 def initialize():
     rospy.init_node("Graphical")
 
     camera_feed = rospy.Subscriber(
-        '{}/{}'.format(name_node_camera, topic_images), image,
-        receive_img)
+        '{}/{}'.format(name_node_camera, topic_images), image, receive_img)
 
     obs_feed = rospy.Subscriber(
-    '{}/{}'.format(name_node_detector_control, topic_observations),
-    observations, receive_observations)
+        '{}/{}'.format(name_node_detector_control, topic_observations),
+        observations, receive_observations)
 
 
 def receive_img(img_message):
     img_data = msg_to_cv2(img_message)
     draw_img(img_data[2])
+
 
 def receive_observations(observations):
     obs_lock.acquire()
@@ -34,11 +34,11 @@ def receive_observations(observations):
     latest_observations = observations
     obs_lock.release()
 
+
 def draw_img(img):
     if not draw_lock.acquire(False):
         print("Previous image drawing still going on!")
         return
-      
 
     height = img.shape[0]
     width = img.shape[1]
@@ -51,7 +51,7 @@ def draw_img(img):
     cv.waitKey(1)
 
     draw_lock.release()
-    
+
 
 def draw_observations(img, height, width):
     for observation in latest_observations.observations:
@@ -62,15 +62,16 @@ def draw_observations(img, height, width):
         bottom = observation.bbox.bottom * height
         cv.putText(
             img, "{} score: {}".format(observation.label,
-                                        round(observation.score, 3)),
-            (int(left), int(top - 5)), cv.QT_FONT_NORMAL, 1, (255, 0, 255),
-            1, cv.LINE_AA)
+                                       round(observation.score,
+                                             3)), (int(left), int(top - 5)),
+            cv.QT_FONT_NORMAL, 1, (255, 0, 255), 1, cv.LINE_AA)
 
         cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)),
-                        (125, 255, 51),
-                        thickness=2)
+                     (125, 255, 51),
+                     thickness=2)
 
     return img
+
 
 if __name__ == "__main__":
     print("started gui")
